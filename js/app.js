29b227b7a99b1e1cc3cb87c885be657251986401
cardiotsunami <cardiotsunami@gmail.com>
2016-10-06 03:16:12 +0600

    fixed with perfect pixel, added button to switch type text/pass

--- js/app.js
+++ js/app.js
@@ -1,9 +1,20 @@
 window.onload = function(){
+	var changeButton = document.getElementById('changeIcon');
+	var inputPass = document.getElementById('passInput');
+	changeButton.onclick = function(){
+		if(inputPass.type == 'password'){
+			inputPass.type = 'text';
+		} else {
+			inputPass.type = 'password';
+		}
+	};
 	var modal = document.getElementById('myModal');
 	var btn = document.getElementById("myBtn");
 	// var span = document.getElementsByClassName("close")[0];
-	btn.onclick = function() {
-	    modal.style.display = "block";
+	if(btn!=null){
+		btn.onclick = function() {
+		modal.style.display = "block";
+		}
 	}
 	// span.onclick = function() {
 	//     modal.style.display = "none";
